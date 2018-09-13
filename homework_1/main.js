1ed3e879b293243c12721bf485061a91af197d6e
Tatiana Grishkina <t.grishkina@drweb.com>
2018-09-13 14:03:48 +0300

    Added homework 1 & 2

---
+++ homework_1/main.js
@@ -0,0 +1,29 @@
+// Первая задача
+
+var Tc, Tf;
+
+Tc = 10;
+
+Tf = (9 / 5) * Tc + 32;
+
+alert(Tf);
+
+
+// Вторая задача
+
+var admin, name;
+
+name = "Василий";
+
+admin = name;
+
+alert(admin);
+
+// Третья задача - будет строка
+
+var n;
+
+n = 1000 + '108';
+
+alert(n);
+alert(n + typeof "n");
